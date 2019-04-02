f5e952faba0ff012557ebfa8e7b237ce685e84bd
Chris Matteson <chris.matteson@gmail.com>
2019-04-02 09:30:11 -0700

    uncomment certificate

--- vault/main.tf
+++ vault/main.tf
@@ -324,22 +324,22 @@ provider azurerm {
   client_secret   = "${azurerm_azuread_service_principal_password.vaultapp.value}"
 }
 
-#resource "azurerm_key_vault_key" "seal" {
-#  provider  = "azurerm.service_principal"
-#  name      = "generated-certificate"
-#  vault_uri = "${azurerm_key_vault.autounseal.vault_uri}"
-#  key_type  = "RSA"
-#  key_size  = 2048
-#
-#  key_opts = [
-#    "decrypt",
-#    "encrypt",
-#    "sign",
-#    "unwrapKey",
-#    "verify",
-#    "wrapKey",
-#  ]
-#}
+resource "azurerm_key_vault_key" "seal" {
+  provider  = "azurerm.service_principal"
+  name      = "generated-certificate"
+  vault_uri = "${azurerm_key_vault.autounseal.vault_uri}"
+  key_type  = "RSA"
+  key_size  = 2048
+
+  key_opts = [
+    "decrypt",
+    "encrypt",
+    "sign",
+    "unwrapKey",
+    "verify",
+    "wrapKey",
+  ]
+}
 
 #provider "vault" {
 #  address = "${null_resource.main.id ? "http://${azurerm_public_ip.main.ip_address}:8200" : null_resource.main.id}"
